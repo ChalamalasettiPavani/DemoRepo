bbce88a172edbd4cc4c6722e17202a2c560260cf
ChalamalasettiPavani <chpavani222@gmail.com>
2018-05-28 22:03:56 +0100

    first git commit for my selenium code

---
+++ src/main/java/com/qa/libs/Test1.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class Test1 {
+
+
+    public void page()
+    {
+        System.out.println("This is Test1 Class");
+    }
+}
---
+++ src/main/java/com/qa/utils/CommonLibrary.java
@@ -0,0 +1,12 @@
+package com.qa.utils;
+
+public class CommonLibrary {
+
+    public void test3(){
+        System.out.println("TEst");
+    }
+
+
+
+
+}
